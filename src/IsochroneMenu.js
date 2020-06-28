84a2221d46803d58e5a405c487ecae1b8bd37bec
Mikey McCormack <michaeljohnmccormack@gmail.com>
2020-06-28 07:43:02 -0400

    Refactor Isochrone menu to a class component

--- src/IsochroneMenu.js
+++ src/IsochroneMenu.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './IsochroneMenu.css'
-import mapboxgl from "mapbox-gl";
-import $ from "jquery";
 
-function IsochroneMenu() {
-    return (
-        <div className='isochrone-menu'>
-            <form id='params'>
-                <h4>Travel mode</h4>
-                <div>
-                    <label>
-                        <input name='profile' type='radio' value='walking'/>
-                        <div>Walking</div>
-                    </label>
-                    <label>
-                        <input name='profile' type='radio' value='cycling'/>
-                        <div>Cycling</div>
-                    </label>
-                    <label>
-                        <input name='profile' type='radio' value='driving'/>
-                        <div>Driving</div>
-                    </label>
-                </div>
-                <h4>Duration</h4>
-                <div>
-                    <label>
-                        <input name='duration' type='radio' value='10'/>
-                        <div>10 min</div>
-                    </label>
-                    <label>
-                        <input name='duration' type='radio' value='20'/>
-                        <div>20 min</div>
-                    </label>
-                    <label>
-                        <input name='duration' type='radio' value='30'/>
-                        <div>30 min</div>
-                    </label>
-                </div>
-            </form>
-        </div>
-    );
+class IsochroneMenu extends Component {
+    render() {
+        return (
+            <div className='isochrone-menu'>
+                <form id='params'>
+                    <h4>Travel mode</h4>
+                    <div>
+                        <label>
+                            <input name='profile' type='radio' value='walking'/>
+                            <div>Walking</div>
+                        </label>
+                        <label>
+                            <input name='profile' type='radio' value='cycling'/>
+                            <div>Cycling</div>
+                        </label>
+                        <label>
+                            <input name='profile' type='radio' value='driving'/>
+                            <div>Driving</div>
+                        </label>
+                    </div>
+                    <h4>Duration</h4>
+                    <div>
+                        <label>
+                            <input name='duration' type='radio' value='10'/>
+                            <div>10 min</div>
+                        </label>
+                        <label>
+                            <input name='duration' type='radio' value='20'/>
+                            <div>20 min</div>
+                        </label>
+                        <label>
+                            <input name='duration' type='radio' value='30'/>
+                            <div>30 min</div>
+                        </label>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default IsochroneMenu;
